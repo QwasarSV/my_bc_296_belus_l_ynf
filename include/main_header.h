0615fef780836dd5cec6238ff89d10fc51ba4bad
lorris BELUS <lorris.belus@icloud.com>
2023-03-05 06:40:55 -0800

    include init

---
+++ include/main_header.h
@@ -0,0 +1,16 @@
+#ifndef __MAIN_HEADERFILE_
+#define __MAIN_HEADERFILE_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdbool.h>
+
+#include <my_string.h>
+#include <my_error_handler.h>
+#include <non_standard_header.h>
+
+
+
+#endif
---
+++ include/my_error_handler.h
@@ -0,0 +1,8 @@
+#ifndef __ERROR_HANDLER_
+#define __ERROR_HANDLER_
+
+#define ARG_SIZE 2
+#define ERR_MSG_MAX_SIZE "YOUR ERROR IS\n"
+
+
+#endif
